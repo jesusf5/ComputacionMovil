cef875d65636b1830f1b58ccb7bd9aa416bb2a04
Angel09-git <angelmaria.calvoj@um.es>
2022-04-13 20:14:42 +0200

    Opcional 2 con avances, Retrofit para solicitar información a la API GEO funcionando.

--- app/src/main/java/com/example/computacionmovil/MapsActivity.java
+++ app/src/main/java/com/example/computacionmovil/MapsActivity.java
@@ -1,42 +1,51 @@
 package com.example.computacionmovil;
 
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
+import static android.content.ContentValues.TAG;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.computacionmovil.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.computacionmovil.databinding.ActivityMapsBinding;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+    //Creamos una variable para el mapa
+    private MapView mMapView;
+
+    //TODO SUPER PROVISIONAL PARA COMPROBAR SI FUNCIONA
+    private GoogleMap gM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //Inicializamos el mapa
+        mMapView = findViewById(R.id.mapView);
+        mMapView.onCreate(null);
+        mMapView.getMapAsync(this);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
     /**
@@ -49,8 +58,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gM = googleMap;
 
         // Add a marker in Sydney and move the camera
         startShowingLocation();
@@ -58,7 +67,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        //mostrarAntenas();
+        mostrarAntenas();
     }
 
     private void startShowingLocation(){
@@ -67,17 +76,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         } else {
-            mMap.setMyLocationEnabled(true);
+            gM.setMyLocationEnabled(true);
         }
     }
 
-    private void mostrarAntenas(){
+    private void mostrarAntenas() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.mylnikov.org/geolocation/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         CellsPosition service = retrofit.create(CellsPosition.class);
-        Call<JSONObject> jsonObjectCall = service.listLocation(1.1, "open", 250, 2, 7840, 200719106);
+        Call<CellsPositionRes> call = service.listLocation(1.1, "open", 250, 2, 7840, 200719106);
+        Log.d("jsonObjectCall", String.valueOf(call));
+        /*
+        Prueba1
+        try {
+            Response<JSONObject> response = jsonObjectCall.execute();
+            JSONObject mainObject = response.body();
+            Log.d("mainObject", String.valueOf(response));
+            TextView texto = findViewById(R.id.textViewXD);
+            texto.setText(jsonObjectCall.toString());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        */
+
+        call.enqueue(new Callback<CellsPositionRes>() {
+            TextView texto = findViewById(R.id.textViewXD);
+            @Override
+            public void onResponse(Call<CellsPositionRes> call, Response<CellsPositionRes> response) {
+                if (!response.isSuccessful()) {
+                    Log.i(TAG, "Error" + response.code());
+                } else {
+                    CellsPositionRes cellsPositions = response.body();
+                    texto.setText(cellsPositions.getData().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CellsPositionRes> call, Throwable t) {
+                Log.e("error", t.toString());
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+        //client.removeLocationUpdates(callback);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
     }
 }
